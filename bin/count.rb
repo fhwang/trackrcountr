2b46156d10685b8c155923e4c9171bfe79a1a984
Francis Hwang <sera@fhwang.net>
2012-12-11 17:07:37 -0500

    Support pagination

--- bin/count.rb
+++ bin/count.rb
@@ -6,16 +6,25 @@ require "#{File.dirname(__FILE__)}/../config"
 
 PivotalTracker::Client.token(USERNAME, PASSWORD)
 project = PivotalTracker::Project.find(PROJECT_ID)
-stories = project.stories.all(
-  state: 'accepted', limit: 10, modified_since: Date.today,
-  includedone: true
-)
+page_size = 5
+offset = 0
 counts_by_date_and_story_type = Hash.new { |outer_hash, date|
   outer_hash[date] = Hash.new(0)
 }
-stories.each do |story|
-  date = story.accepted_at.to_date
-  counts_by_date_and_story_type[date][story.story_type] += 1
+stories = project.stories.all(
+  state: 'accepted', limit: page_size, modified_since: Date.parse(START_DATE),
+  includedone: true, offset: offset
+)
+while !stories.empty?
+  stories.each do |story|
+    date = story.accepted_at.to_date
+    counts_by_date_and_story_type[date][story.story_type] += 1
+  end
+  offset += page_size
+  stories = project.stories.all(
+    state: 'accepted', limit: page_size, modified_since: Date.parse(START_DATE),
+    includedone: true, offset: offset
+  )
 end
 dates = counts_by_date_and_story_type.keys.sort
 story_types = %w(bug chore feature release)
--- config.example.rb
+++ config.example.rb
@@ -1,4 +1,5 @@
 USERNAME = 'jane.doe@example.com'
 PASSWORD = 'mypivotaltrackerpassword'
 PROJECT_ID = 123456
+START_DATE = '2012-11-15'
 

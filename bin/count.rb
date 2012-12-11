3807f20d62723ec34c9415a8271a077852414ee3
Francis Hwang <sera@fhwang.net>
2012-12-11 17:19:44 -0500

    Simple objectyness

--- bin/count.rb
+++ bin/count.rb
@@ -3,39 +3,10 @@
 require 'csv'
 require 'pivotal-tracker'
 require "#{File.dirname(__FILE__)}/../config"
+require "#{File.dirname(__FILE__)}/../lib/trackrcountr/report"
 
-PivotalTracker::Client.token(USERNAME, PASSWORD)
-project = PivotalTracker::Project.find(PROJECT_ID)
-page_size = 5
-offset = 0
-counts_by_date_and_story_type = Hash.new { |outer_hash, date|
-  outer_hash[date] = Hash.new(0)
-}
-stories = project.stories.all(
-  state: 'accepted', limit: page_size, modified_since: Date.parse(START_DATE),
-  includedone: true, offset: offset
+report = Trackrcountr::Report.new(
+  USERNAME, PASSWORD, PROJECT_ID, Date.parse(START_DATE)
 )
-while !stories.empty?
-  stories.each do |story|
-    date = story.accepted_at.to_date
-    counts_by_date_and_story_type[date][story.story_type] += 1
-  end
-  offset += page_size
-  stories = project.stories.all(
-    state: 'accepted', limit: page_size, modified_since: Date.parse(START_DATE),
-    includedone: true, offset: offset
-  )
-end
-dates = counts_by_date_and_story_type.keys.sort
-story_types = %w(bug chore feature release)
-CSV do |csv_out|
-  csv_out << [''] + story_types
-  dates.each do |date|
-    csv_row = [date.to_s]
-    counts = counts_by_date_and_story_type[date]
-    story_types.each do |story_type|
-      csv_row << counts[story_type]
-    end
-    csv_out << csv_row
-  end
-end
+report.collect
+puts report.to_csv
---
+++ lib/trackrcountr/report.rb
@@ -0,0 +1,48 @@
+module Trackrcountr
+  class Report
+    def initialize(username, password, project_id, start_date)
+      PivotalTracker::Client.token(username, password)
+      @project = PivotalTracker::Project.find(project_id)
+      @start_date = start_date
+      @counts_by_date_and_story_type = Hash.new { |outer_hash, date|
+        outer_hash[date] = Hash.new(0)
+      }
+    end
+
+    def collect
+      page_size = 5
+      offset = 0
+      stories = @project.stories.all(
+        state: 'accepted', limit: page_size, 
+        modified_since: @start_date, includedone: true, offset: offset
+      )
+      while !stories.empty?
+        stories.each do |story|
+          date = story.accepted_at.to_date
+          @counts_by_date_and_story_type[date][story.story_type] += 1
+        end
+        offset += page_size
+        stories = @project.stories.all(
+          state: 'accepted', limit: page_size,
+          modified_since: @start_date, includedone: true, offset: offset
+        )
+      end
+    end
+
+    def to_csv
+      dates = @counts_by_date_and_story_type.keys.sort
+      story_types = %w(bug chore feature release)
+      CSV.generate do |csv_out|
+        csv_out << [''] + story_types
+        dates.each do |date|
+          csv_row = [date.to_s]
+          counts = @counts_by_date_and_story_type[date]
+          story_types.each do |story_type|
+            csv_row << counts[story_type]
+          end
+          csv_out << csv_row
+        end
+      end
+    end
+  end
+end
